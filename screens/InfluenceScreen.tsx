83448339324ed7e5e582a37e775eb0c3301448db
Ethan Haezebroeck <haezebroecke@gmail.com>
2020-09-02 12:59:33 -0500

    Implemented wireframe Influence page with static data and set up imports for scraping web data from govtrack.com

--- screens/InfluenceScreen.tsx
+++ screens/InfluenceScreen.tsx
@@ -1,14 +1,50 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 
 export default function InfluenceScreen() {
+  const state = 'Iowa';
+  const streetAddress = '8302 NW Chevalia Dr.';
+
+  // rep static test data
+  const president = 'Donald Trump';
+  const senatorOne = 'Chuck G';
+  const senatorTwo = 'Joni E';
+  const houseReps = [
+    {name: 'Abby Finkenauer', district: 1},
+    {name: 'Dave Loebsack', district: 2},
+    {name: 'Cindy Axne', district: 3}, 
+    {name: 'Steve King', district: 4}
+  ];
+
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Influence</Text>
+      <Text style={styles.mainHeader}>Federal Government</Text>
+      <Text style={styles.subHeader}>President:</Text>
+      <Text>{president}</Text>
+      <Text style={styles.subHeader}>Senators:</Text>
+      <Text>{senatorOne}</Text>
+      <Text>{senatorTwo}</Text>
+      <Text style={styles.subHeader}>House Represenatives:</Text>
+      <FlatList 
+        style={styles.centeredList}
+        data={houseReps}
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Text>{item.district}</Text>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/InfluenceScreen.tsx" />
     </View>
@@ -22,7 +58,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
   },
   separator: {
@@ -30,4 +66,13 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  mainHeader: {
+    fontSize: 25,
+  },
+  subHeader: {
+    fontSize: 18,
+  },
+  centeredList: {
+    textAlign: 'center',
+  }
 });
